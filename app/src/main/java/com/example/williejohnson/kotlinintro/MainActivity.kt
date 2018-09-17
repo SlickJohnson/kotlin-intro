4030568b1f0a4dcc7b2e06a222da3a2ab1fbd08e
SlickJohnson <liwa.johnson@gmail.com>
2018-09-17 11:09:34 -0700

    Implement toast button

--- app/src/main/java/com/example/williejohnson/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/williejohnson/kotlinintro/MainActivity.kt
@@ -3,22 +3,17 @@ package com.example.williejohnson.kotlinintro
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-//        val test = "test"
-//        Log.d("onCdreate", test)
-        for (i in 0..30) {
-            if (i % 15 == 0) {
-                Log.i("FizzBuzz", i.toString())
-            } else if (i % 5 == 0) {
-                Log.i("Buzz", i.toString())
-            } else if (i % 3 == 0) {
-                Log.i("Fizz", i.toString())
-            }
-        }
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
